e73a95b79f4e4187099d2403eb37eb579f594264
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-10-26 19:56:06 +0530

    feat(auth): implement secure login flow with access and refresh tokens
    - Added backend login logic using JWT-based authentication
    - Integrated access and refresh token generation for session management
    - Ensured secure response structure and error handling

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,13 +1,15 @@
 import User from "../model/userModel.js";
 import bcrypt from "bcrypt";
-import { generateAndStoreToken } from "../utils/tokenService.js";
+import { generateAccessToken, generateAndStoreToken, generateRefreshToken } from "../utils/tokenService.js";
 import { sendVerificationEmail } from "../utils/sendEmail.js";
 import { checkLoginAttempts, logFailedLoginAttempt } from "../utils/MonitorFailedLoginAttempt.js";
+import { ENV } from "../config/env.js";
 
 
 export const signUp = async (req, res) => {
 
     try {
+
         //destructuring form data
         const { name, email, password, confirmPassword, role, phone } = req.body;
 
@@ -35,52 +37,99 @@ export const signUp = async (req, res) => {
         const response = await newUser.save();
 
         //generating token with crypto
-        const token = await generateAndStoreToken(response._id)
+        const token = await generateAndStoreToken(response._id);
 
         //basic skeleton of the verification link send to user
         const verificationLink = `http://localhost:5000/api/user/verify-email/${response._id.toString()}/${token}`;
 
         //sending verification Email to user
         await sendVerificationEmail(email, verificationLink);
-
-        res.status(201).json({ success: true, message: "Signup successful.! verify email to continue" })
+        res.status(201).json({ success: true, message: "Signup successful.! verify email to continue" });
 
     } catch (error) {
 
-        console.log(error)
-        res.status(500).json({ success: false, message: "Internal Server error while signing Up" })
+        console.log(error);
+        res.status(500).json({ success: false, message: "Internal Server error while signing Up" });
     }
 }
 
 export const loginUser = async (req, res) => {
-    const { email, password } = req.body
 
-    if (!email || !password)
-        return res.status(400).json({ success: false, message: "Email and password are required" })
+    try {
+        const { email, password } = req.body;
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email))
-        return res.status(400).json({ success: false, message: "Invalid email format" })
+        if (!email || !password)
+            return res.status(400).json({ success: false, message: "Email and password are required" });
 
-    const user = await User.findOne({ email })
-    if (!user)
-        return res.status(400).json({ success: false, message: "Invalid credentials" })
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email))
+            return res.status(400).json({ success: false, message: "Invalid email format" })
 
-    if (!user.isVerified)
-        return res.status(400).json({ success: false, message: "Verify Your email before Login" })
+        const user = await User.findOne({ email })
+        if (!user)
+            return res.status(400).json({ success: false, message: "Invalid credentials" })
 
-    if (user.status === "suspended" || user.status === "banned")
-        return res.status(400).json({ success: false, message: "Your account has been suspended" })
+        if (!user.isVerified)
+            return res.status(400).json({ success: false, message: "Verify Your email before Login" })
 
-    const isValidPassword = await bcrypt.compare(password, user.passwordHash)
-    if (!isValidPassword) {
-        await logFailedLoginAttempt(user._id, req.ip)
-        return res.status(400).json({ success: false, message: "Invalid Credentials" })
+        if (user.status === "suspended" || user.status === "banned")
+            return res.status(400).json({ success: false, message: "Your account has been suspended" })
+
+        const isValidPassword = await bcrypt.compare(password, user.passwordHash)
+        if (!isValidPassword) {
+            await logFailedLoginAttempt(user.email, req.ip)
+            return res.status(400).json({ success: false, message: "Invalid Credentials" })
+        }
+
+        const recentFailedAttempts = await checkLoginAttempts(user._id, req.ip)
+        if (recentFailedAttempts > 5) {
+            return res.status(400).json({ success: false, message: "Too many failed Attempts, Try againj in 15 Minutes!" })
+        }
+        const accessToken = await generateAccessToken(user._id, user.email, user.role);
+
+        const refreshToken = await generateRefreshToken(user._id);
+
+        //since no default value in schema
+        user.refreshTokens = user.refreshTokens || [];
+        user.refreshTokens.push({
+            token: refreshToken,
+            createdAt: Date.now(),
+            expiresAt: Date.now() + 7 * 24 * 60 * 60 * 1000, // 7 days
+            userAgent: req.headers['user-agent'],
+            ipAddress: req.ip
+        });
+
+        if (user.refreshTokens.length > 0) {
+            user.refreshTokens = user.refreshTokens.slice(-5)
+        }
+
+        user.lastLogin = Date.now();
+
+        const response = await user.save();
+
+        if (response) {
+            res.cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                secure: ENV.NODE_ENV,
+                sameSite: 'strict',
+                maxAge: 7 * 24 * 60 * 60 * 1000 //7 days
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Login successful",
+            accessToken,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                avatar: user.avatar
+            }
+        })
+    } catch (error) {
+        console.log("Error while loggin in:", err)
+        res.status(500).json({ success: false, message: "Internal server error while login" })
     }
-
-    const recentFailedAttempts = await checkLoginAttempts(user._id, req.ip)
-    if (recentFailedAttempts > 5) {
-        return res.status(400).json({ success: false, message: "Too many failed Attempts, Try againj in 15 Minutes!" })
-    }
-
 }
