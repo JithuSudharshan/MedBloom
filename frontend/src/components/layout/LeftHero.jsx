bb33797a9e9d343d8f2fe9db302e3478ca0fb0fb
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-10-27 16:28:20 +0530

    refactor(fronend): make LeftHero component reusable with dynamic image loading based on user role

--- frontend/src/components/layout/LeftHero.jsx
+++ frontend/src/components/layout/LeftHero.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const LeftHero = ({ sentence, imgSrc }) => (
-    <div className="hidden md:flex flex-col justify-center items-center bg-gradient-to-br from-[#016666] to-[#01CCCB] text-white p-10 w-1/2">
-        <img src={imgSrc} alt="hero" className=" mb-8" />
-        <h2 className="text-2xl font-bold mb-2">Join MEDBLOOM Today</h2>
+const LeftHero = ({ headLine, sentence, imgSrc, imgSize }) => (
+    <div className="hidden md:flex flex-col  justify-center items-center bg-gradient-to-br from-[#016666] to-[#01CCCB] text-white p-10 w-1/2">
+        <img src={imgSrc} alt="hero" className={`rounded-4xl mb-8 ${imgSize}`} />
+        <h2 className="text-2xl font-bold mb-2">{headLine}</h2>
         <p className="text-sm text-center max-w-sm">
             {sentence}
         </p>
