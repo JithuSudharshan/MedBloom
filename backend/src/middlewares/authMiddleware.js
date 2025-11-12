3956b2f351f7a0e749ead66c910c679206c1b493
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-11-13 00:57:29 +0530

    feat(auth): add JWT authentication and role-based authorization middleware
    - Implemented authenticate middleware to verify JWT access tokens
    - Added support for handling expired and invalid tokens
    - Attached decoded user details (id, email, role) to request object
    - Implemented authorize middleware for role-based access control
    - Sends 'requiresRefresh' flag when access token is expired

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,68 @@
+// middleware/auth.js
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+
+export const authenticate = async (req, res, next) => {
+    try {
+        // 1. Get token from Authorization header
+        const authHeader = req.headers.authorization
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({
+                success: false,
+                message: 'Access token required'
+            })
+        }
+        const token = authHeader.split(' ')[1]
+
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+        } catch (error) {
+            if (error.name === 'TokenExpiredError') {
+                return res.status(401).json({
+                    success: false,
+                    message: 'Access token expired',
+                    requiresRefresh: true //Sending Signal frontend to refresh
+                })
+            }
+
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid access token'
+            })
+        }
+
+        req.user = {
+            userId: decoded.userId,
+            email: decoded.email,
+            role: decoded.role
+        }
+        next()
+    } catch (error) {
+        console.error('Authentication error:', error)
+        return res.status(500).json({
+            success: false,
+            message: 'Authentication failed'
+        })
+    }
+};
+
+export const authorize = (...allowedRoles) => {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.status(401).json({
+                success: false,
+                message: 'Unauthorized - No user info'
+            });
+        }
+
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({
+                success: false,
+                message: `Access denied. Only ${allowedRoles.join(', ')} can access this resource.`
+            });
+        }
+
+        next();
+    };
+};
