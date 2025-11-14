0c27e391a2187c90c4578df2acbc29d3abd2fd59
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-11-15 02:27:14 +0530

    feat: implemented backend controller to create and update new password
    Added backend controller logic for handling new password creation and password updates, including validation, token checks, and updating the user record in the database.

---
+++ backend/src/controller/auth/createPasswordController.js
@@ -0,0 +1,66 @@
+import User from "../../model/userModel.js"
+import { ENV } from "../../config/env.js"
+import bcrypt from 'bcrypt'
+
+
+export const createPassword = async (req, res) => {
+    try {
+        const { email, password, confirmPassword } = req.body
+        console.log(email, password, confirmPassword)
+
+        if (!email)
+            return res.status(400).json({
+                success: false,
+                message: "Email fields cannot be empty"
+            })
+
+        if (!password || !confirmPassword)
+            return res.status(400).json({
+                success: false,
+                message: "Passwword field cannot be empty"
+            })
+
+        if (password !== confirmPassword)
+            return res.status(400).json({
+                success: false,
+                message: "Passwords must match"
+            })
+
+        if (password.length > 64) {
+            return res.status(400).json({
+                success: false,
+                message: "Password cannot exceed 64 characters"
+            })
+        }
+        const user = await User.findOne({ email }).select('+passwordHash')
+
+        if (!user)
+            return res.status(400).json({
+                success: false,
+                message: "User doesn't exist"
+            })
+
+        if (!user.isVerified)
+            return res.status(400).json({
+                success: false,
+                message: "Email not verified"
+            })
+
+        const hashedPassword = await bcrypt.hash(password, Number(ENV.SALTROUND))
+
+        user.passwordHash = hashedPassword
+        await user.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Password reset successfully. Please login with your new password"
+        });
+
+    } catch (error) {
+        console.log("Something went wrong", error)
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        })
+    }
+}
