8c463da79b4caadb7e44337cef7b493a4bc02158
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-11-26 01:47:26 +0530

    feat: create patient model schema
    Added new patient schema with required fields and validations to support patient data storage and management.

---
+++ backend/src/model/patientModel.js
@@ -0,0 +1,96 @@
+import mongoose from 'mongoose';
+
+const patientSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    profile_url: {
+        type: String,
+        default: "https://media.istockphoto.com/id/1451587807/vector/user-profile-icon-vector-avatar-or-person-icon-profile-picture-portrait-symbol-vector.jpg?s=612x612&w=0&k=20&c=yDJ4ITX1cHMh25Lt1vI1zBn2cAKKAlByHBvPJ8gEiIg=" // Profile picture storage URL or path
+    },
+    emergencyNumber: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['female', 'male', 'intersex', 'other'],
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    bloodType: {
+        type: String,
+        enum: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'],
+        required: true
+    },
+    cholesterol: {
+        type: String // Consider more specific type if you need numeric
+    },
+    height: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true
+    },
+    bloodPressure: {
+        type: String // Store as string like "120/80 mmHg"
+    },
+    glucoseLevel: {
+        type: String
+    },
+    allergies: {
+        type: String,
+        default: ''
+    },
+    medicalCondition: {
+        type: String,
+        default: ''
+    },
+    smoking: {
+        type: String,
+        enum: [
+            'Never smoked',
+            'Former smoker',
+            'Occasionally (social/rarely)',
+            'Yes, daily',
+            'Yes, but not daily'
+        ],
+        required: true
+    },
+    drinking: {
+        type: String,
+        enum: [
+            'Never',
+            'Quit drinking',
+            'Occasionally (social/rarely)',
+            'Yes, regularly',
+            'Yes, but not regularly'
+        ],
+        required: true
+    },
+    Food_or_Drug_Intolerances: {
+        type: String,
+        default: ''
+    },
+    Mental_Health_History: {
+        type: String,
+        default: ''
+    }
+}, {
+    timestamps: true
+});
+
+const Patient = mongoose.model('Patient', patientSchema);
+
+export default Patient;
