2aebae09bc3c8e014134188b4235c335eb2aaf29
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-11-28 22:04:23 +0530

    feat(ui): align password input with input field component design

--- frontend/src/components/form/PasswordInput.jsx
+++ frontend/src/components/form/PasswordInput.jsx
@@ -4,16 +4,21 @@ const PasswordInput = ({ label, register, name, error, placeholder }) => {
     const [show, setShow] = useState(false);
 
     return (
-        <div className="flex flex-col gap-1 mb-3 relative">
-            <label className="text-gray-500 text-xs font-medium">{label}</label>
+        <div className="mb-6 relative">
+            {label && (
+                <label className="text-sm text-teal-700 mb-2 font-medium block">
+                    {label}
+                </label>
+            )}
             <input
                 type={show ? "text" : "password"}
-                {...register(name, { required: `${label} is required` })}
+                {...register(name)}
                 placeholder={placeholder}
-                className="border border-gray-300 text-gray-500  bg-white rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-teal-400"
+                className={`w-full px-4 py-3 text-gray-600 rounded-lg border focus:outline-none focus:ring-2 focus:ring-teal-400
+          ${error ? "border-red-400" : "border-gray-200"} text-sm bg-white`}
             />
             <span
-                className="absolute right-3 top-8 text-sm text-gray-500 cursor-pointer"
+                className="absolute right-3 top-11 text-sm text-gray-500 cursor-pointer"
                 onClick={() => setShow(!show)}
             >
                 {show ? "Hide" : "Show"}
