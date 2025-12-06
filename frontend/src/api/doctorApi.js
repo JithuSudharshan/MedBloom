99efa73864e9bd37b6caacdf220f12b9a75ea0f9
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-12-07 01:29:08 +0530

    feat: add frontend API integration to submit basic and professional onboarding data as FormData

--- frontend/src/api/doctorApi.js
+++ frontend/src/api/doctorApi.js
@@ -2,10 +2,30 @@ import api from "./axiosInstance";
 
 export const loadDoctorData = async () => {
     try {
-        const response = await api.get("/docotor/profile")
+        const response = await api.get("/doctor/profile")
         return response
     } catch (error) {
         console.log("Error while fetching doctor details", error)
+        throw error
+    }
+}
+
+export const doctorbasicOnboarding = async (formData) => {
+    try {
+        const response = await api.post('/doctor/onboarding/basic', formData)
+        return response
+    } catch (error) {
+        console.log("Error while Dr basic onboarding", error)
+        throw error
+    }
+}
+
+export const doctorProfessionalOnboarding = async (formData) => {
+    try {
+        const response = await api.post('doctor/onboarding/proffesional', formData)
+        return response
+    } catch (error) {
+        console.log("Error while Dr proffesional onboarding", error)
         throw error
     }
 }
