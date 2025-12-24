8bce64859d5712786f000ace075cbc79cb677cee
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-12-25 01:28:49 +0530

    feat(admin-api): implement admin routes for viewing, editing, blocking and unblocking users

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,13 +1,46 @@
 import express from 'express'
-import { loginAdmin } from "../controller/adminControllers/adminAuth/adminLoginController.js";
 import { logoutAdmin } from '../controller/adminControllers/adminAuth/adminLogoutController.js';
+import upload from '../config/multer.js';
+import {
+    approveDoctor,
+    rejectDoctor,
+    fetchApprovedDcotorsDetails,
+    fetchApprovedDoctorList,
+    fetchDoctorsDetailsToEdit,
+    fetchPendingDoctorList,
+    editDoctorsProfile,
+    blockDoctorProfile,
+    unblockDoctorProfile
+} from '../controller/adminControllers/adminDashboard/adminDashboardController.js';
+import {
+    editPatientsProfile,
+    fetchPatientDetailsToEdit,
+    fetchPatientPorfile,
+    fetchTotlPatients
+} from '../controller/adminControllers/adminDashboard/adminPatientControllers.js';
 
 const router = express.Router()
 
 //AdminRoutes
-router.post("/login", loginAdmin)
 router.post("/logout", logoutAdmin)
+router.get("/doctors/pending", fetchPendingDoctorList)
+router.get("/doctors/approved", fetchApprovedDoctorList)
+router.patch("/doctors/:id/approve", approveDoctor)
+router.patch("/doctors/:id/reject", rejectDoctor)
+router.get('/doctors/approved/:id/details', fetchApprovedDcotorsDetails);
+router.get('/doctor/:id/to-edit', fetchDoctorsDetailsToEdit);
+router.patch("/doctor/block/:id", blockDoctorProfile)
+router.patch("/doctor/unblock/:id", unblockDoctorProfile)
+router.get("/patients", fetchTotlPatients)
+router.get("/patient/:id/profile", fetchPatientPorfile)
+router.get("/patient/:id/edit", fetchPatientDetailsToEdit)
+router.patch('/patient/:id/edit-profile', upload.none(), editPatientsProfile)
 
+router.patch(
+    "/doctors/:id/edit-profile",
+    upload.none(),
+    editDoctorsProfile
+);
 
-export default router
+export default router;
 
