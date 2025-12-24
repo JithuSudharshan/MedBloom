d40a456b10481972eb1e63e48493a703b6891407
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-12-25 01:07:52 +0530

    feat(backend): add Socket.IO configuration and event handling in socket.js

---
+++ backend/src/config/socket.IO.js
@@ -0,0 +1,27 @@
+import { Server } from 'socket.io';
+
+let io;
+
+export const initSocket = (server) => {
+    io = new Server(server, {
+        cors: {
+            origin: ['http://localhost:5173'],
+            credentials: true
+        },
+    })
+
+    io.on('connection', (socket) => {
+        const userId = socket.handshake.query.userId;
+        if (userId) {
+            socket.join(userId.toString())
+        }
+        socket.on('disconnect', () => { });
+    });
+}
+
+export const getIO = () => {
+    if (!io) {
+        throw new Error('Socket.IO not initialized!')
+    }
+    return io
+}
