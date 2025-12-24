3a66d511fc2907532b4a8cbde94c2991816dea84
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-12-25 01:06:58 +0530

    feat(backend): initialize Socket.IO server using HTTP server wrapper

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import connectDB from "./config/db.js";
 import { ENV } from "./config/env.js";
 import app from "./app.js";
 import { connectRedis } from "./config/redisClient.js";
+import http from 'http';
+import { initSocket } from './config/socket.IO.js';
 
 //connects to DataBase
 connectDB()
@@ -9,9 +11,12 @@ connectDB()
 //connect to redis
 connectRedis()
 
+//connect to socket
+const server = http.createServer(app);
+initSocket(server);
 
-app.listen(ENV.PORT, () => {
+server.listen(ENV.PORT, () => {
     console.log("===============================")
     console.log(`Server listening to PORT ${ENV.PORT}`)
     console.log("===============================")
-})
+});
