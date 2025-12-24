e71975fb8492bdd4bb79f0dbe07fd5f335b173a4
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-12-24 21:55:46 +0530

    feat(admin): add AdminPatientDetailsPage to view patient details from admin dashboard with back navigation

---
+++ frontend/src/pages/admin/dashboard/AdminPatientDetailsPage.jsx
@@ -0,0 +1,23 @@
+
+import { useNavigate, useParams } from "react-router-dom";
+import AdminPatientDetails from "../../../components/profile/admin/patientProfile/AdminPatientDetails";
+import Navbar from "../../../components/landing page/Navbar";
+import ProfileBanner from "../../../components/profile/ProfileBanner";
+
+const AdminPatientDetailsPage = () => {
+    const { patientId } = useParams();
+    const navigate = useNavigate()
+    console.log(patientId)
+    return (
+        <>
+            <Navbar />
+            <ProfileBanner profileOwner={"Admin Profile"} profileDescription={"Manage the patients information and records"} />
+            <div className="min-h-screen max-w-7xl mx-auto w-full">
+                <AdminPatientDetails patientId={patientId} onBack={() => navigate("/admin/dashboard")} />;
+            </div>
+        </>
+
+    )
+};
+
+export default AdminPatientDetailsPage
